c54b7836965376b6baa42fa710d9c522dc7cf125
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2023-11-19 21:48:04 +0330

    - Set up DataBase

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from store.models import Product
+
 
 def say_hello(request):
+    query_set = Product.objects.all()
+
+    print(query_set[0])
+
     return HttpResponse("hello")
 
 
