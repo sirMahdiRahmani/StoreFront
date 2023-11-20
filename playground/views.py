6608c6ea3f183348bd029378c0d7f4c995883e18
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2023-11-20 21:00:31 +0330

    - ORM Session

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from store.models import Product
+from store.models import Product, Customer, Collection, Order, OrderItem
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
+    query_set = OrderItem.objects.filter(product__collection__id=3)
 
-    print(query_set[0])
-
-    return HttpResponse("hello")
+    return render(request, 'hello.html', {'name': "Mahdi", "orderitems": list(query_set)})
 
 
 def summary(request):
