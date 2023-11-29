fa410c7e20b9b109c51f67407e0d79974955c0ce
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2023-11-29 21:45:09 +0330

    - Working With Expression Wrapper

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.db.models import Value, F, Func, Count, Max, Min, Sum, ExpressionWrapper, DecimalField
+from django.db.models.functions import Concat
 from store.models import Product, Customer, Collection, Order, OrderItem
 
 
 def say_hello(request):
-    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set').order_by('-placed_at')[:5]
-
-    return render(request, 'hello.html', {'name': "Mahdi", "orders": list(queryset)})
+    result = Product.objects.annotate(
+        total_sell=Sum(
+            F('orderitem__unit_price') *
+            F('orderitem__quantity')
+        )
+    ).filter(total_sell__isnull=False).order_by('-total_sell')[0:5]
+    return render(request, 'hello.html', {'name': "Mahdi", "result": result})
