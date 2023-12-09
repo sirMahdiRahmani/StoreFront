71645727ab651fa416f4b3c470b859253dbb4815
sir_mahdi <sir.mahdirahmani@gmail.com>
2023-12-09 21:50:43 +0330

    - Some change to modify on fedora

--- store/models.py
+++ store/models.py
@@ -12,7 +12,7 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+        'Product', on_delete=models.SET_NULL, null=True, blank=True, related_name='+')
 
     def __str__(self) -> str:
         return self.title
--- store/views.py
+++ store/views.py
@@ -6,4 +6,7 @@ from store.models import Product, Customer, Collection, Order, OrderItem, Cart, 
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': "Mahdi"})
+    queryset = Product.objects.annotate(
+        discounted_price=ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField()))
+
+    return render(request, 'hello.html', {'name': "Mahdi", "data": list(queryset)})
